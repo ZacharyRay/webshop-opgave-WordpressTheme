9fd4e0b64e2de2956fc71ae60db9b4da956817d1
ZacharyRay <47400499+ZacharyRay@users.noreply.github.com>
2020-05-06 10:58:17 +0200

    created acf environment

--- templates/main-template.php
+++ templates/main-template.php
@@ -11,11 +11,36 @@ get_header();
 
 
 
-<!-- ACF: -->
+<!-- acf-elements & woo-elements: -->
 
-            <?php //hero ?>
-      <?php if( get_row_layout() == 'hero' ):?>
-          <?php get_template_part('acf-elements/acf', 'hero'); ?>
+            <?php //frontpage hero ?>
+      <?php if( get_row_layout() == 'frontpage_hero' ):?>
+          <?php get_template_part('acf-elements/acf', 'frontpage-hero'); ?>
+      <?php endif; ?>
+
+      <?php //frontpage categories (woocommerce) ?>
+      <?php if( get_row_layout() == 'frontpage_categories' ):?>
+        <?php get_template_part('woo-elements/woo', 'frontpage-categories'); ?>
+      <?php endif; ?>
+
+      <?php //frontpage usp ?>
+      <?php if( get_row_layout() == 'frontpage_usp' ):?>
+        <?php get_template_part('acf-elements/acf', 'frontpage-usp'); ?>
+      <?php endif; ?>
+
+      <?php //frontpage on sale (woocommerce) ?>
+      <?php if( get_row_layout() == 'frontpage_on_sale' ):?>
+        <?php get_template_part('woo-elements/woo', 'frontpage-on-sale'); ?>
+      <?php endif; ?>
+
+      <?php //frontpage banner ?>
+      <?php if( get_row_layout() == 'frontpage_banner' ):?>
+        <?php get_template_part('acf-elements/acf', 'frontpage-banner'); ?>
+      <?php endif; ?>
+
+      <?php //frontpage recommended (woocommerce) ?>
+      <?php if( get_row_layout() == 'frontpage_recommended' ):?>
+        <?php get_template_part('woo-elements/woo', 'frontpage-recommended'); ?>
       <?php endif; ?>
 
 <?php
@@ -24,9 +49,11 @@ get_header();
 ?>
 
 
+
+
 <!-- Woo -->
 
-<?php get_template_part('woo-elements/product', 'category'); ?>
+
 
 <?php get_template_part('woo-elements/product', 'display'); ?>
 
---
+++ woo-elements/woo-frontpage-on-sale.php
@@ -0,0 +1,1 @@
+<div>on sale virker!</div>
