5c086b34fe5503842d06a085b03bf63eeff1da96
ZacharyRay <47400499+ZacharyRay@users.noreply.github.com>
2020-05-12 12:48:43 +0200

    Footer links dynamic

--- functions.php
+++ functions.php
@@ -78,3 +78,23 @@ add_theme_support( 'custom-logo', array(
     'flex-width' => true,
     'header-text' => array( 'site-title', 'site-description' ),
    ) );
+
+
+   //options page
+
+add_action('acf/init', 'my_acf_op_init');
+function my_acf_op_init() {
+
+    // Check function exists.
+    if( function_exists('acf_add_options_page') ) {
+
+        // Register options page.
+        $option_page = acf_add_options_page(array(
+            'page_title'    => __('Options'),
+            'menu_title'    => __('Options'),
+            'menu_slug'     => 'options',
+            'capability'    => 'edit_posts',
+            'redirect'      => false
+        ));
+    }
+}
