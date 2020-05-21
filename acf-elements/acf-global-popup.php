37cb642de6c725071832f82af760d7221523e553
ZacharyRay <47400499+ZacharyRay@users.noreply.github.com>
2020-05-21 10:57:29 +0200

    popup email subscription

---
+++ acf-elements/acf-global-popup.php
@@ -0,0 +1,14 @@
+<!-- mPopup box -->
+<div id="mpopupBox" class="mpopup">
+    <!-- mPopup content -->
+    <div class="mpopup-content">
+        <div class="mpopup-head">
+            <span class="close">×</span>
+            <h2>Subscribe To Our Newsletter</h2>
+        </div>
+        <div class="mpopup-main">
+            <p><input type="text" id="email" placeholder="Enter your email"/></p>
+            <p><input type="submit" value="SUBSCRIBE"/></p>
+        </div>
+    </div>
+</div>
